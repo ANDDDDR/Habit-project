24abf728e3e763d3d0bc8aa6f82d241fd1174f9b
Andrey JsJune <muravev488@gmail.com>
2025-01-31 14:45:45 +0300

    рендер меню

--- scripts/app.js
+++ scripts/app.js
@@ -1,22 +1,55 @@
 'use strict'
 
-/*
-	1. разница вар лет
-	2. разница слайс сплайс
-	3. метод every some
-	4. регулярные выражения для строк
-	5. json.stringify parce
-	6. нюансы глубокого копирования
-	7. опшинал чейнинг
-	8. стек
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
+// page
+const page = {
+	menu: document.querySelector('.menu__list')
+};
+// data
+function loadData(){
+	const habbitString = localStorage.getItem(HABBIT_KEY);
+	const habbitArray = JSON.parse(habbitString);
+	if (Array.isArray(habbitArray)){
+		habbits = habbitArray;
+	}
+}
 
-	1. функция высшего порядка
-	2. использование стрелочных в методах вместо обычных
-	3. какая должна быть запись функции если надо получать неизвестное количество аргументов
-	4. можно использовать рест оператор в аргументах функции?
-	5. использование методов обьекта без свойтва
-	6. как правильно выносить методы за обьекты и как часто используют кол и аплай
-	7. как правильно обрабатывать события через джс или через атрибуты в штмл
-
-
- */
+function saveData(){
+	localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
+// render
+function renderMenu(activeHabbit){
+	if(!activeHabbit){
+		return;
+	}
+	for(const habbit of habbits){
+		const existed = document.querySelector(`[menu-habbit-id="${habbit.id}"]`);
+		if(!existed){
+			const element = document.createElement('button');
+			element.setAttribute('menu-habbit-id', habbit.id);
+			element.classList.add('menu__item');
+			element.addEventListener('click', ()=> rerender(habbit.id));
+			element.innerHTML = `<img src="/images/${habbit.icon}.svg" alt="${habbit.name}" >`;
+			if(activeHabbit.id === habbit.id){
+				element.classList.add('menu__item_active');
+			}
+			page.menu.appendChild(element);
+			continue;
+		}
+		if(activeHabbit.id === habbit.id){
+			existed.classList.add('menu__item_active');
+		}else{
+			existed.classList.remove('menu__item_active');
+		}
+	}
+}
+function rerender(activeHabbitId){
+	const activeHabbit = habbits.find(habbit=>habbit.id === activeHabbitId);
+	renderMenu(activeHabbit);
+}
+// init
+(()=>{
+	loadData();
+	rerender(habbits[0].id);
+})()
